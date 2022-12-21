4f9ee4d620b2e0a2eea3f0b4570dc2b982234e74
404 Racer <2238976035@qq.com>
2022-12-21 12:51:59 +0900

    RGBA+Mask

--- main.cpp
+++ main.cpp
@@ -13,36 +13,37 @@
 using namespace cv;
 using namespace std;
 
-struct depthUnit {
+struct depthPlane {
 	int depth;
 	int number;
 };
 
-bool compareByNumber(const depthUnit& a, const depthUnit& b)
+bool compareByNumber(const depthPlane& a, const depthPlane& b)
 {
 	return a.number > b.number;
 }
 
-bool compareByDepth(const depthUnit& a, const depthUnit& b)
+bool compareByDepth(const depthPlane& a, const depthPlane& b)
 {
 	return a.depth > b.depth;
 }
 
 void main() {
+	/*
 	Mat ref = imread("./refImage.png");
-	Mat mask = imread("./mask.png", IMREAD_GRAYSCALE);
+	Mat mask = imread("./depth.png", IMREAD_GRAYSCALE);
 	Mat dst;
 	cv::inpaint(ref, mask, dst, 1, INPAINT_NS);
 	imshow("result", dst);
 	waitKey(0);
+	*/
 
-	/*
 	int baseline = 50;
 	int cameraDistance = 500;
 	Mat depth = imread("./depth.png");
 	Mat ref = imread("./refImage.png");
 
-	vector<depthUnit> disparityArray;
+	vector<depthPlane> disparityArray;
 	for (int width = 0; width < depth.cols; width++) {
 		for (int height = 0; height < depth.rows; height++) {
 			int Z = depth.at<Vec3b>(height, width)[0];
@@ -63,13 +64,15 @@ void main() {
 	sort(disparityArray.begin(), disparityArray.end(), compareByDepth);
 
 	for (int i = 0; i < disparityArray.size(); i++) {
-		Mat MPI = Mat(ref.rows, ref.cols, CV_8UC3, Scalar(0, 0, 0));
+		Mat MPI = Mat(ref.rows, ref.cols, CV_8UC4, Scalar(0, 0, 0, 0));
 		int syntheticSize = baseline * (disparityArray[i].depth - disparityArray[disparityArray.size()-1].depth) / cameraDistance;
-		printf("%i\n", disparityArray.size());
 		for (int width = 0; width < depth.cols; width++) {
 			for (int height = 0; height < depth.rows; height++) {
 				if (depth.at<Vec3b>(height, width)[0] == disparityArray[i].depth) {
-					MPI.at<Vec3b>(height, width) = ref.at<Vec3b>(height, width);
+					MPI.at<Vec4b>(height, width) = Vec4b(ref.at<Vec3b>(height, width)[0], ref.at<Vec3b>(height, width)[1], ref.at<Vec3b>(height, width)[2],255);
+				}
+				else if (depth.at<Vec3b>(height, width)[0] > disparityArray[i].depth) {
+					MPI.at<Vec4b>(height, width) = Vec4b(255,255,255,255);
 				}
 			}
 		}
@@ -77,5 +80,4 @@ void main() {
 		oss << "MPI/" << disparityArray[i].depth << ".png";
 		imwrite(oss.str(), MPI);
 	}
-	*/
 }
